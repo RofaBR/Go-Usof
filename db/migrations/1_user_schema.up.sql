dd5c1a8b5bfef67925960163323fd2ee92ebdb6e
RofaBR <rostislavbrigine@gmail.com>
2025-12-16 18:07:05 +0200

    feat: add initial user schema migration

---
+++ db/migrations/1_user_schema.up.sql
@@ -0,0 +1,12 @@
+CREATE TYPE user_role AS ENUM ('user', 'admin');
+
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    login VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    fullname VARCHAR(255) NOT NULL,
+    rating INT NOT NULL DEFAULT 0,
+    role user_role NOT NULL DEFAULT 'user',
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+);
